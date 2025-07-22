8ee91495bbd4346c59c7c9841f3394818bb7bd6b
Muhammad Ali <malitariq5324@gmail.com>
2025-07-22 19:08:42 +0500

    Server Setup - Without Debugging

--- server.py
+++ server.py
@@ -1,19 +1,33 @@
 from socket import *
+from threading import *
 
 def connect_client():
     while True:
         client_socket,client_address = server.accept()
         client_socket.send(b"Send your Name!")
-        client_msg = client_socket.recv(BUFFER_SIZE).decode()
+        client_msg = client_socket.recv(BUFFER_SIZE).decode(ENCODE_DECODER)
         clients_sockets.append(client_socket)
         clients_names.append(client_msg)
         client_socket.send(f"{client_msg}, Congrats you're connected the the server!".encode(ENCODE_DECODER))
+        broad_cast(f"# --- {client_msg} has joined the chat! --- #")
 
-def broad_cast(msg):
-    pass
+        rec_msg = Thread(target=receive_msg,args=(client_socket,))
+        rec_msg.start()
+
+def broad_cast(msg,*args):
+    for client in clients_sockets:
+        client_nam = clients_names[clients_sockets.index(client)]
+        client.send(f"{client_nam}: {msg}")
 
 def receive_msg(client_socket):
-    pass
+    try:
+        client_msg = client_socket.recv(BUFFER_SIZE).decode(ENCODE_DECODER)
+        broad_cast(client_msg)
+    except:
+        broad_cast(f"# --- {clients_names[clients_sockets.index(client_socket)]} left the chat! --- #")
+        clients_sockets.remove(client_socket)
+        clients_names.pop(clients_sockets.index(client_socket))
+        client_socket.close()
 
 # -- Constants -- #
 HOST_IP = gethostbyname(gethostname())
@@ -32,4 +46,5 @@ server.bind((HOST_IP,HOST_PORT))
 server.listen()
 print("Server Listening.....")
 
-connect_client()
+while True:
+    connect_client()
